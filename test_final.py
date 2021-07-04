305a09cea4e5ca5df021aad08bc5a7c79a7f1801
Jiwoo.Choi <joos@JiwooChoiui-MacBookAir.local>
2021-07-04 16:45:44 +0900

    피라미드 완성

--- test_final.py
+++ test_final.py
@@ -1,18 +1,24 @@
 def down():
     for b in range(10):
         for i in range(b + 1):
-            print("*", end='')
+            print("*", end = '')
         print("")
 
 def up():
     for b in range(10):
         for i in range(10 - b):
-            print("*", end='')
+            print("*", end = '')
         print("")
 
 def diamond():
 	pass
 
 def pyramid():
-	pass
+	for i in range (10): 
+        for a in range(i + 1):
+            print("*", end = '')
+        print("")
+
+pyramid()
+
 
