bd6b08252c50defc2c1de7f46d35f3b18e527dfb
Jiwoo.Choi <joos@JiwooChoiui-MacBookAir.local>
2021-07-11 14:05:18 +0900

    lint

--- pyramid.py
+++ pyramid.py
@@ -1,31 +1,35 @@
 def down():
     for b in range(10):
         for i in range(b + 1):
-            print("*", end = '')
+            print("*", end='')
         print()
+
 
 def up():
     for b in range(10):
         for i in range(10 - b):
-            print("*", end = '')
+            print("*", end='')
         print()
+
 
 def pyramid():
     for a in range(10):
         for c in range(10 - a):
-            print(' ', end = '')
+            print(' ', end='')
         for b in range(2 * a + 1):
-            print("*", end = '')
+            print("*", end='')
         print()
+
 
 def reversePy():
     for a in range(11):
         for c in range(a):
-            print(' ', end = '')
+            print(' ', end='')
         for b in range(20 - (2 * a - 1)):
-            print("*", end = '')
+            print("*", end='')
         print()
-    
+
+
 def diamond():
     pyramid()
     reversePy()
