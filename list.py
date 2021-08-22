16425e9e41f53c30fa8ac40ee768e7cadc3763e0
Jiwoo.Choi <joos@JiwooChoiui-MacBookAir.local>
2021-08-22 15:52:37 +0900

    003.1

--- list.py
+++ list.py
@@ -46,55 +46,72 @@ def generatePrimenumber(N):
     return i
 
 
-#print(generatePrimenumber(N))
+# print(generatePrimenumber(N))
 
 
-#N=int(input())
+# N=int(input())
 
 def reverselist(N):
-    x=list()
+    x = list()
     for i in range(N):
         x.append(N-i)
     return x
 
-#print(reverselist(N))
+# print(reverselist(N))
 
 
-Test = [5,4,3,2,1]
+Test = [5, 4, 3, 2, 1]
 
-#n=int(input())
+# n=int(input())
+
 
 def remove(a, n):
-    a[n-1]=-1
-    
+    a[n-1] = -1
 
-case = [6,7,8,10]
+
+case = [6, 7, 8, 10]
 
 #remove(case, n)
-#print(case)
+# print(case)
 
 
-#1. reverslist함수 안에 remove넣기
-#N=int(input())
-#M=int(input())
+# 1. reverslist함수 안에 remove넣기
+# N=int(input())
+# M=int(input())
 
 def reverselist_remove(N, M):
-    x=list()
+    x = list()
     for i in range(N):
         x.append(N-i)
-    x[M-1]=-1
+    x[M-1] = -1
     return x
 
 #print(reverselist(N, M))
 
-#2. reverselist함수 안에 remove함수 넣기
-N=int(input())
-M=int(input())
+
+# 2. reverselist함수 안에 remove함수 넣기
+N = int(input())
+M = int(input())
+
+
 def reverselist_remove_2(N, M):
-    x=list()
+    x = list()
     for i in range(N):
         x.append(N-i)
     remove(x, M)
     return x
 
-print(reverselist_remove_2(N, M))
+
+#print(reverselist_remove_2(N, M))
+
+# 003.1 과제
+# 0은 A로, 1은 B로 바꿔서 인코딩하는 함수. 치환불가 수는 error.
+# abcdefghijklmnopqrstuvwxyz(25)
+
+b = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+a = [0, 1, 1]
+
+# 리스트 a에 있는 요소가 리스트 B의 자리값의 알파벳을 불러오면 됨.
+
+print(a)
