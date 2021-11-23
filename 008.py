44ca474ea3c63643f21a32bc9886e9e3d68c5442
agnes0304 <jiwoochoi0304@gmail.com>
2021-11-23 23:03:18 +0900

    ongoing

--- 008.py
+++ 008.py
@@ -25,24 +25,21 @@ def makerandomlist(n):
 agnes = [70, 80, 10, 3, 54, 1, 30, 99, 21, 65, 47]
 
 def bubble(r, od):
-    if od != "asc":
-        if od != "dsc":
-            print("asc/dsc 중에 하나를 입력하세요")
-        else:
-            for i in range(len(r)-1):   # 비교하는 횟수 하나씩 줄일라고 쓴거야
-                for j in range(len(r)-1-i):
-                    if r[j] > r[j+1]:
-                        r[j], r[j+1] = r[j+1], r[j]
-            r.reverse()
-    elif od == "asc":
-        for i in range(len(r)-1):
-            for j in range(len(r)-1-i):
-                if r[j] > r[j+1]:
-                    r[j], r[j+1] = r[j+1], r[j]
+    od = od.lower()
+
+    if od != "asc" and od != "desc":
+            print("asc/desc 중에 하나를 입력하세요")
+    
+    for i in range(len(r)-1):   # 비교하는 횟수 하나씩 줄일라고 쓴거야
+        for j in range(len(r)-1-i):
+            if od == "asc" and r[j] > r[j+1]:
+                r[j], r[j+1] = r[j+1], r[j]
+            elif od == "desc" and r[j] < r[j+1]:
+                r[j], r[j+1] = r[j+1], r[j]
 
     return r
 
-# print(bubble(agnes, "sc"))
+# print(bubble(agnes, "asc"))
 
 
 # 008.3
@@ -68,8 +65,137 @@ def targetfind(r, t):
         if r[i] == t:
             print(i+1, "번째에 있음")
 
-r = makerandomlist(100000)
-start = time.time()
-targetfind(r, 100)
-end = time.time()
-print(f"{end-start:.5f}초")
+# r = makerandomlist(100000)
+# start = time.time()
+# targetfind(r, 100)
+# end = time.time()
+# print(f"{end-start:.5f}초")
+
+
+# 008.5
+# insertion sort 
+# 아놔 모르겠는데
+# 내가 비교할거를 앞에서부터 비교해서 맞는자리에 끼워넣는거야. 2번은 1번, 3번은 1,2번, 4번은 1,2,3번
+# 내가 비교할 애가 i면 i-1번 비교해야 함. 
+
+agnes = [70, 80, 10, 3, 54, 1, 30, 99, 21, 65, 47]
+
+def insertion(r, od):
+    od = od.lower()
+
+    if od != "asc" and od != "desc":
+        print("asc/desc 중에 하나만 입력")
+
+    for i in range(len(r)):
+        for j in range(i+1):
+            if od == "desc" and r[i] > r[j]:
+                r[i], r[j] = r[j], r[i]
+            elif od == "asc" and r[i] < r[j]:
+                r[i], r[j] = r[j], r[i]
+    return r
+
+# 저 이게 insertion을 생각하고 작성하긴 했는데 실제 insertion인지...코드가 bubble sort랑 너무 비슷해서요..
+
+# print(insertion(agnes, "asc"))
+
+
+# 008.6
+# 퀵
+# pivot기준으로 나눠. pivot보다 작은거, pivot, pivot보다 큰거. 
+# 그놈의 recursive.
+# 일단 랜덤으로 피봇이 정해지고 새 리스트가 있어. 
+# 그 리스트에 맨 처음부터 비교를 해서 피봇보다 작은거 부터 append를 해. 
+
+agnes = [70, 80, 10, 3, 54, 1, 30, 99, 21, 65, 47]
+
+
+
+# 008.7
+# dashlane, PW generator 만들어
+# 사용자가 length, digit, letters, symbols 인풋으로. 
+# 아 아스키 그냥 그 테이블에서 선택못하나.
+
+
+def dashlane(length, dig=True, let=True, sym=True):
+    pw_temp = list()
+    digit = [0,1,2,3,4,5,6,7,8,9]
+    
+    letters = list()
+    for i in range(65, 91):
+        letters.append(chr(i))
+    for i in range(97, 123):
+        letters.append(chr(i))
+
+    symbols = list()
+    for i in range(33, 48):
+        symbols.append(chr(i))
+    for i in range(58, 65):
+        symbols.append(chr(i))
+    for i in range(91, 97):
+        symbols.append(chr(i))
+    for i in range(123, 127):
+        symbols.append(chr(i))
+
+
+    if dig == True and let == True and sym == True:
+        i = length // 3
+        r = length % 3
+
+        for _ in range(i):
+            pw_temp.append(random.choice(digit))
+        
+        for _ in range(i):
+            pw_temp.append(random.choice(letters))
+
+        for _ in range(i+r):
+            pw_temp.append(random.choice(symbols))
+
+    elif dig == True and let == True:
+        i = length // 2
+        r = length % 2
+
+        for _ in range(i):
+            pw_temp.append(random.choice(digit))
+
+        for _ in range(i+r):
+            pw_temp.append(random.choice(letters))
+
+
+    elif dig == True and sym == True:
+        i = length//2
+        r = length % 2
+        for _ in range(i):
+            pw_temp.append(random.choice(digit))
+
+        for _ in range(i+r):
+            pw_temp.append(random.choice(symbols))
+    
+
+    elif let == True and sym == True:
+        i = length//2
+        r = length % 2
+
+        for _ in range(i):
+            pw_temp.append(random.choice(letters))
+
+        for _ in range(r+i):
+            pw_temp.append(random.choice(symbols))
+
+    else:
+        print("length 이후엔 True/False를 각각 입력, 최소 2개 이상 True")
+    
+    random.shuffle(pw_temp)
+
+    pw = str()
+    for i in range(len(pw_temp)):
+        if pw_temp[i] != str():
+            pw += str(pw_temp[i])
+        else:
+            pw += pw_temp[i]
+
+    print(pw)
+
+dashlane(20, True, True, True)
+dashlane(20, True, True, False)
+dashlane(31, True, True, True)
+dashlane(31, True, True, False)
