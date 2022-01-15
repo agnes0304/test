fb8ff555b22d65b29017afda0fb46972a6670f98
agnes0304 <jiwoochoi0304@gmail.com>
2022-01-15 17:49:02 +0900

    ongoing

--- multi_chat_client.py
+++ multi_chat_client.py
@@ -1,17 +1,29 @@
 import socket
+import threading
+
+# sv 에서 broadcast 듣기/ msg sending
 
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-c.connect(('127.0.0.1', 1111))
-while True:
-    l = input()
-    if l != "exit":
-        c.send(l.encode('utf-8'))
+c.connect(('127.0.0.1', 10000))
+signal = False
 
+
+def listener(c):
+    while True and not signal:
         message = c.recv(4096)
         if message:
             print(message.decode('utf-8'))
 
-    else: 
+
+t = threading.Thread(target=listener, args=(c,))
+t.start()
+
+
+while True:
+    l = input()
+    if l != "exit":
+        c.send(l.encode('utf-8'))
+    else:
+        signal = True
         c.send("exit".encode('utf-8'))
-        c.close()
         break
--- multi_chat_server.py
+++ multi_chat_server.py
@@ -4,39 +4,40 @@ import socket
 import threading
 
 sv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sv.bind(('127.0.0.1', 1111))
+sv.bind(('127.0.0.1', 10000))
 sv.listen()
 
 # client마다 X/ 기능별 thread
 
 clients = []
 
-def broadcast(message):
+
+def broadcast(message, me):
     # send to all clients
     for c in clients:
-        c.send(message)
+        if c != me:
+            c.send(message)
 
-    if message == b"exit":
-        clients.remove(c)
-        c.close()
-
-def handler(c):
-    print("thread", id)
+def handler(cs):
     while True:
+        message = cs.recv(4096)
+        if message == b"exit":
+            clients.remove(cs)
+            cs.close()
+            return
+
         print(message)
-        broadcast(message)
+        broadcast(message, cs)
 
 # accept c1 - 'c1 connected'print - append c1 clients[] - recv c1 message - thread t1 start
-# thread; handler(print'thread id' - print c1 msg - func broadcast())
+# thread; handler(print c1 msg - func broadcast())
 
 while True:
     c, addr = sv.accept()
     print(f"{addr} has been connected")
     clients.append(c)
 
-    message = c.recv(4096)
-
-    t = threading.Thread(target=handler, args=(message))
+    t = threading.Thread(target=handler, args=(c,))
     t.start()
 
 sv.close()
