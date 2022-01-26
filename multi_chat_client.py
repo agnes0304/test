c3318066446defd322c7ca70f02a1b2901414f89
agnes0304 <jiwoochoi0304@gmail.com>
2022-01-26 14:05:17 +0900

    ongoing

--- multi_chat_client.py
+++ multi_chat_client.py
@@ -3,32 +3,27 @@ import socket
 import threading
 
 # sv 에서 broadcast 듣기/ msg sending
-# def listener(c):
-#     while True and not signal:
-#         message = c.recv(4096)
-#         if message:
-#             print(message.decode('utf-8'))
+def listener(c):
+    while True and not signal:
+        message = c.recv(4096)
+        if message:
+            print(message.decode('utf-8'))
 
-clients = []
-for _ in range(3000):
-    clients.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+# 접속가능한 max client
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect(('127.0.0.1', 10000))
 
-for c in clients:
-    c.connect(('127.0.0.1', 10000))
-    # print(c.getsockname)
-    # signal = False
+print(c.getsockname)
+signal = False
 
-    # t = threading.Thread(target=listener, args=(c,))
-    # t.start()
-
-    # while True:
-    #     l = input()
-    #     if l != "exit":
-    #         c.send(l.encode('utf-8'))
-    #     else:
-    #         signal = True
-    #         c.send("exit".encode('utf-8'))
-    #         break
+t = threading.Thread(target=listener, args=(c,))
+t.start()
 
 while True:
-    pass
+    l = input()
+    if l != "exit":
+        c.send(l.encode('utf-8'))
+    else:
+        signal = True
+        c.send("exit".encode('utf-8'))
+        break
