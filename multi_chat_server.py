41e5ae569cb06aa551eeaa8d4778a8ea7ef76cfe
agnes0304 <jiwoochoi0304@gmail.com>
2022-01-26 14:04:36 +0900

    ongoing_select

--- multi_chat_server.py
+++ multi_chat_server.py
@@ -2,20 +2,18 @@
 
 import socket
 import threading
+import select
 
 sv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sv.bind(('127.0.0.1', 10000))
 sv.listen()
 
-# client마다 X/ 기능별 thread
-
-clients = []
-
+connection_list=[sv]
 
 def broadcast(message, me):
     # send to all clients
-    for c in clients:
+    for c in connection_list:
         if c != me:
             c.send(message)
 
@@ -24,22 +22,27 @@ def handler(cs, addr):
         message = cs.recv(4096)
         if message == b"exit":
             print(f'{addr} out')
-            clients.remove(cs)
+            connection_list.remove(cs)
             cs.close()
             return
 
         print(message)
         broadcast(message, cs)
 
-# accept c1 - 'c1 connected'print - append c1 clients[] - recv c1 message - thread t1 start
+# accept c1 - 'c1 connected'print - append c1 connection_list[] - recv c1 message - thread t1 start
 # thread; handler(print c1 msg - func broadcast())
 
 while True:
-    c, addr = sv.accept()
-    print(f"{addr} has been connected")
-    clients.append(c)
+    read, write, error = select.select(connection_list, [], [])
+    
+    for cs in read:
+        if cs == sv:
+            c, addr = sv.accept()
+            print(f"{addr} has been connected")
+            connection_list.append(c)
 
-    t = threading.Thread(target=handler, args=(c, addr))
-    t.start()
+        else: 
+            t = threading.Thread(target=handler, args=(c, addr))
+            t.start()
 
 sv.close()
